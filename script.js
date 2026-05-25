d582024fdbf05494b57b39831b0a4660a02ea29b
Diego Dhaier <145449672+ddhaier@users.noreply.github.com>
2026-05-25 09:54:24 +0100

    Generate 10 by 10 hexagon board with JavaScript

--- script.js
+++ script.js
@@ -2,5 +2,33 @@
 const board = document.getElementById("board");
 const gameMessage = document.getElementById("gameMessage");
 
-//Thid message confirms that JavaScript is connected to the page
-gameMessage.textContent = "JavaScript is connected. The board will be creted next.";
+// Board size
+const rows = 10;
+const columns = 10;
+
+//This function creates the full board
+function createBoard() {
+    for (let row = 0; row < rows; row++) {
+        const rowElement = document.createElement("div");
+        rowElement.classList.add("hex-row");
+
+        for (let column = 0; column < columns; column++) {
+            const hex = document.createElement("div");
+            hex.classList.add("hex");
+
+            hex.dataset.row = row;
+            hex.dataset.column = column;
+
+            rowElement.appendChild(hex);
+        }
+
+        board.appendChild(rowElement);
+
+    }
+
+    gameMessage.textContent = "The 10x10 hexagon board has been created.";
+
+}
+
+//Start the game board
+createBoard();

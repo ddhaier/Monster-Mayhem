dbd566a8f9a3fab71b924ca6ca349cbf0ab9076d
Diego Dhaier <145449672+ddhaier@users.noreply.github.com>
2026-05-25 09:17:56 +0100

    Connect JavaScript file to HTML page

---
+++ script.js
@@ -0,0 +1,6 @@
+//Get elements from the HTML
+const board = document.getElementById("board");
+const gameMessage = document.getElementById("gameMessage");
+
+//Thid message confirms that JavaScript is connected to the page
+gameMessage.textContent = "JavaScript is connected. The board will be creted next.";
